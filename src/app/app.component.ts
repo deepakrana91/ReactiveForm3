b1400301d889f5808e30b690251a31e9902562c6
= <=>
2017-09-22 15:53:13 +0530

    Angular ReactiveForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { FormdataService } from './formdata.service';
+import { FormService } from './form.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title = 'Simple Form';
+  desigArray:any[]=[];
+  nameArray:any[]=[];
+  signupForm:FormGroup;
+  selectedprefix="Mr";
+
+
+  constructor(private formService:FormService,private formDataservice:FormdataService){}
+
+  ngOnInit(){
+
+    this.desigArray= this.formService.title
+    this.signupForm = new FormGroup({
+      'fname': new FormControl(null,Validators.required),
+      'lname': new FormControl(null,Validators.required),
+      'pnumber': new FormControl(null,Validators.required),
+      'prefix': new FormControl(null,Validators.required)
+    })
+  }
+
+  
+
+  
+  onSubmit(){
+
+    let user={
+      fname:this.signupForm.value['fname'],
+      lname:this.signupForm.value['lname'],
+      phone:this.signupForm.value['pnumber'],
+      designation:this.signupForm.value['prefix'],
+    }
+  
+    this.formDataservice.addData(user);
+    this.nameArray= this.formDataservice.getData();
+    
+
+  }
 }
---
+++ src/app/form.service.ts
@@ -0,0 +1,11 @@
+
+
+export class FormService {
+  
+    constructor() { }
+  
+    title : any [] = [{id:1,value:'Mr'},{id:2,value:'Mrs'}];
+    
+  
+  }
+  
